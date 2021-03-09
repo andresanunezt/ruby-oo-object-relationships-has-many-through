a6077485fe7d4144ebd58ad117dedf7990fad509
Andrés Nuñez <andresanunez98@gmail.com>
2021-03-09 14:58:53 -0500

    commit tree 510a50b2bd32981e1ad3a0afa67b26e2eaef8b9e

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+attr_accessor :customer, :waiter, :tip, :total
+@@all = []
+def initialize(waiter,customer,total,tip=0)
+@customer = customer
+@waiter = waiter
+@tip = tip
+@total = total
+@@all << self
+end
+
+def self.all
+    @@all
+end
 
 end
